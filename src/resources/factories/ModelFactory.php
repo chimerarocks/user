bdee4d301583fbdf7aa98febc706aa482cb5d9e3
chimerarocks <joaopedrodslv@gmail.com>
2016-08-27 10:52:45 -0300

    fix

--- src/resources/factories/ModelFactory.php
+++ src/resources/factories/ModelFactory.php
@@ -14,8 +14,8 @@
 $factory->define(\ChimeraRocks\User\Models\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
+        'email' => 'user@email.com',
+        'password' => bcrypt(123456),
         'remember_token' => str_random(10),
     ];
 });
